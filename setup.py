f533e97bf5fd2558c23e3f0048dab42b37a24071
afrkciymjt <115087366+afrkciymjt@users.noreply.github.com>
2022-11-28 15:07:44 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Januus RiskReport',
-      version='0.1',
+      version='0.2',
       description='Free & robust tool for checking the risk-history of crypto addresses',
       author='Januus',
       packages=['januus_riskreport'],
