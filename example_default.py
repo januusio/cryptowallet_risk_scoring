8660288143b8fdfb97e803ddf87690cb73e868df
mattyjacks <98132678+mattyjacks@users.noreply.github.com>
2022-11-30 13:30:43 -0500

    updated example with 2 print statements

--- example_default.py
+++ example_default.py
@@ -3,10 +3,11 @@ from januus_riskreport.client import riskreport_on_entity
 eth_address_1 = "0xbb0ea877a85df253ccc312b80c644da31443abfd"
 
 # Single address example
+print("Running single address example...")
 report = riskreport_on_entity(eth_addresses=[eth_address_1])
 print("Scoring breakdown for " + eth_address_1)
 print("Combined risk: " + str(report.risk_scores.combined_risk))
 print("Fraud risk: " + str(report.risk_scores.fraud_risk))
 print("Reputation risk: " + str(report.risk_scores.reputation_risk))
 print("Lending risk: " + str(report.risk_scores.lending_risk))
-
+print("Example running complete. Congratulations, you're now ready to reduce risk!")
