081f77bc42dd7958de8386df5e8e5946f20a422f
afrkciymjt <115087366+afrkciymjt@users.noreply.github.com>
2023-03-14 14:05:53 -0400

    Update multi_example_default.py

--- multi_example_default.py
+++ multi_example_default.py
@@ -1,18 +1,18 @@
 from januus_riskreport.client import riskreport_on_entity 
+# In this example, we are scoring an entity that owns multiple wallets
 
 eth_address_1 = "0xbb0ea877a85df253ccc312b80c644da31443abfd"
 eth_address_2 = "0x690bfe31f17b22924e7f0037b0b0046dcc63cc44"
+
 eth_addresses=[eth_address_1, eth_address_2]
-# Single address example
 report = riskreport_on_entity(eth_addresses=eth_addresses)
-print("Scoring breakdown for:")
-for eth_address in eth_addresses:
-    print(eth_address)
-print()
+
+print("Addresses owned by entity:", eth_addresses)
+print("Score results:\n")
 print("Combined risk: " + str(report.risk_scores.combined_risk))
-print("Fraud risk: " + str(report.risk_scores.fraud_risk))
-print("Reputation risk: " + str(report.risk_scores.reputation_risk))
-print("Lending risk: " + str(report.risk_scores.lending_risk))
+print("\tFraud risk: " + str(report.risk_scores.fraud_risk))
+print("\tReputation risk: " + str(report.risk_scores.reputation_risk))
+print("\tLending risk: " + str(report.risk_scores.lending_risk))
 
 for reason in report.reasons:
     print()
