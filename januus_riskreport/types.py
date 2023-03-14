09a375aca1122220486292ce71e98ebb0730b5a1
afrkciymjt <115087366+afrkciymjt@users.noreply.github.com>
2023-03-14 13:10:58 -0400

    Add files via upload

--- januus_riskreport/types.py
+++ januus_riskreport/types.py
@@ -58,6 +58,11 @@ class RiskOffsets:
     def as_json(self)->Dict[str, Any]:
         return self.__as_json__ 
 
+
+
+
+
+
 @dataclass 
 class RiskElaboration:  
     @staticmethod 
@@ -80,8 +85,11 @@ class RiskElaboration:
             return VerifiedDates.from_dict(d)        
         
         else: 
-            raise AttributeError(f"Unknown label: {label}")
+            return UnknownElaboration.from_dict(d)
 
+
+    def __summerize__(self)->Optional[SummerizedInteractions]:
+        return None
 
 @dataclass 
 class IsBadActor(RiskElaboration):
@@ -98,6 +106,13 @@ class IsBadActor(RiskElaboration):
     def as_json(self)->Dict[str, Any]:
         return self.__as_json__ 
 
+    def __summerize__(self)->Optional[SummerizedInteractions]:
+        ts = TagSummary(address="", tagged_as=[])
+        for d in self.risk_details:
+            ts["address"] = d["address"]
+            for factor in d["risk_factors"]:
+                ts["tagged_as"].append(factor["actor_type"])
+        return SummerizedInteractions(percentage_of_category=100.0, bad_nbrs=[ts])
 @dataclass 
 class RiskReason:
     explanation: str 
@@ -138,6 +153,15 @@ class BadNeighborDetails:
     def as_json(self)->Dict[str, Any]:
         return self.__as_json__ 
 
+class TagSummary(TypedDict):
+    address: str
+    tagged_as: List[str]
+
+class SummerizedInteractions(TypedDict):
+    percentage_of_category: float
+    bad_nbrs: List[TagSummary]
+
+
 @dataclass 
 class SentToBadActor(RiskElaboration):
     how_many_recipients: int
@@ -149,14 +173,25 @@ class SentToBadActor(RiskElaboration):
     @raise_as("SentToBadActor")
     def from_dict(d: Dict[str, Any]   )->SentToBadActor:
         return SentToBadActor(
-            how_many_recipients = d["how_many_recipients"],
-            how_many_bad_recipients = d["how_many_bad_recipients"],
+            how_many_recipients = int(d["how_many_recipients"]),
+            how_many_bad_recipients = int(d["how_many_bad_recipients"]),
             bad_recipient_details = [BadNeighborDetails.from_dict(dd) for dd in d["bad_recipient_details"]],
             __as_json__=d
         )
 
     def as_json(self)->Dict[str, Any]:
         return self.__as_json__ 
+
+
+    def __summerize__(self)->Optional[SummerizedInteractions]:
+        bad_nbrs = []
+        for nbr in self.bad_recipient_details:
+            details = nbr.risk_details
+            bad_nbrs.append(TagSummary(address = nbr.recipient,
+                                       tagged_as = [x["actor_type"] for x in details["risk_factors"]]))
+        return SummerizedInteractions(percentage_of_category= 100.0 * self.how_many_bad_recipients / self.how_many_recipients,
+                                      bad_nbrs=bad_nbrs)
+
 
 @dataclass 
 class FundedByBadActor(RiskElaboration):
@@ -169,11 +204,24 @@ class FundedByBadActor(RiskElaboration):
     @raise_as("FundedByBadActor")
     def from_dict(d: Dict[str, Any]   )->FundedByBadActor: 
         x = FundedByBadActor(**d)
+        x.how_many_bad_funders = int(x.how_many_bad_funders)
+        x.how_many_funders = int(x.how_many_funders)
         x.__as_json__ = d
         return x 
 
     def as_json(self)->Dict[str, Any]:
         return self.__as_json__ 
+
+    def __summerize__(self)->Optional[SummerizedInteractions]:
+        bad_nbrs = []
+        for nbr in self.bad_funder_details:
+            assert isinstance(nbr, Dict)
+            details = nbr["risk_details"]
+            bad_nbrs.append(TagSummary(address = details["address"],
+                                       tagged_as = [x["actor_type"] for x in details["risk_factors"]]))
+        return SummerizedInteractions(percentage_of_category= 100.0 * self.how_many_bad_funders / self.how_many_funders,
+                                      bad_nbrs=bad_nbrs)
+
 
 @dataclass 
 class VerifiedDate:
@@ -186,6 +234,7 @@ class VerifiedDate:
     @raise_as("VerifiedDate")
     def from_dict(d: Dict[str, Any]   )->VerifiedDate:
         x = VerifiedDate(**d)
+        x.weight = float(x.weight)
         x.__as_json__ = d
         return x 
 
@@ -208,21 +257,47 @@ class VerifiedDates(RiskElaboration):
         return self.__as_json__ 
 
 @dataclass 
+class UnknownElaboration(RiskElaboration):
+    __as_json__: Dict[str, Any] = field(default_factory=lambda: {}) 
+
+    @staticmethod 
+    @raise_as("UnknownElaboration")
+    def from_dict(d:Dict[str,Any])->RiskElaboration:
+       x = UnknownElaboration()
+       x.__as_json__ = d
+       return x 
+
+    def as_json(self)->Dict[str, Any]:
+        return self.__as_json__ 
+
+@dataclass 
 class BadZeroValuedTxs(RiskElaboration):
     how_many_neighbors: int 
     how_many_bad_neighbors: int 
-    bad_neighbor_details: List[Dict[str,str]]
+    bad_neighbor_details: List[Dict[str,Any]]
     __as_json__: Dict[str, Any] = field(default_factory=lambda: {}) 
     @staticmethod
     @raise_as("BadZeroValuedTxs")
     def from_dict(d: Dict[str, Any]   )->BadZeroValuedTxs:
         x = BadZeroValuedTxs(**d)
         x.__as_json__ = d
+        x.how_many_neighbors = int(x.how_many_neighbors)
+        x.how_many_bad_neighbors = int(x.how_many_bad_neighbors)
         return x 
 
     def as_json(self)->Dict[str, Any]:
         return self.__as_json__ 
 
+
+    def __summerize__(self)->Optional[SummerizedInteractions]:
+        bad_nbrs = []
+        for nbr in self.bad_neighbor_details:
+            assert isinstance(nbr, Dict)
+            details = nbr["risk_details"]
+            bad_nbrs.append(TagSummary(address = details["address"],
+                                       tagged_as = [x["actor_type"] for x in details["risk_factors"]]))
+        return SummerizedInteractions(percentage_of_category= 100.0 * self.how_many_bad_neighbors / self.how_many_neighbors,
+                                      bad_nbrs=bad_nbrs)
 
 
 @dataclass 
@@ -242,4 +317,12 @@ class RiskReport:
         )
     
     def as_json(self)->Dict[str, Any]:
-        return self.__as_json__ 
+        return self.__as_json__
+
+    def summerize_risky_interactions(self)->Dict[str, SummerizedInteractions]:
+        ret = {}
+        for reason in self.reasons:
+            sww = reason.risk_elaboration.__summerize__()
+            if sww is not None:
+               ret[reason.label] = sww
+        return ret
