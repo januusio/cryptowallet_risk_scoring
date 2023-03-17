c5d9c0c97532fb2b3088bbedccee862cd80a03a7
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2023-03-17 09:53:30 -0400

    Replaced empty `print()` with newline `\n`

--- example_reasons.py
+++ example_reasons.py
@@ -6,6 +6,5 @@ eth_address_1 = "0xbb0ea877a85df253ccc312b80c644da31443abfd"
 report = riskreport_on_entity(eth_addresses=[eth_address_1])
 
 for reason in report.reasons:
-    print()
-    print(f"The following contributed to the score by: {reason.offsets.combined_risk_offset}")
+    print(f"\nThe following contributed to the score by: {reason.offsets.combined_risk_offset}")
     print(reason.explanation)
